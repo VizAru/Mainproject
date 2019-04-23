58588284fb0461589dd1e9f54595818dcf510747
aleena1995 <aleenawilson95@gmail.com>
2019-04-23 20:25:43 +0530

    reg and login

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+   include("config.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = $_POST['email'];
+      $mypassword = $_POST['psw']; 
+      
+      $sql = "SELECT name FROM register WHERE email = '$myusername' and password = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['user'] = $myusername;
+         header("location: lhome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
